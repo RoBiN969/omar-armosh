abb639c762b0726c6c179de0fefb68c9f353aa1d
robeenkhnefes <robeenkhnefes@localhost>
2024-11-23 16:00:07 +0200

    4th commit (javadocs) for the shareddata class

--- ter2/src/SharedData.java
+++ ter2/src/SharedData.java
@@ -1,45 +1,93 @@
 import java.util.ArrayList;
 
-public class SharedData 
-{
-	private ArrayList<Integer> array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
+/**
+ * SharedData is a shared resource class used by multiple threads.
+ * It contains an array of integers, a target value, and synchronization variables 
+ * to communicate between threads.
+ */
+public class SharedData {
+    /**
+     * The array of integers to be processed.
+     */
+    private ArrayList<Integer> array;
 
-	public SharedData(ArrayList<Integer> array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+    /**
+     * Tracks which elements in the array contribute to the solution.
+     */
+    private boolean[] winArray;
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+    /**
+     * A flag indicating whether a solution has been found.
+     */
+    private boolean flag;
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    /**
+     * The target value to check for the subset sum.
+     */
+    private final int b;
 
-	public ArrayList<Integer> getArray() 
-	{
-		return array;
-	}
+    /**
+     * Constructs a SharedData object with the given array and target value.
+     *
+     * @param array The array of integers to be shared among threads.
+     * @param b     The target value for the subset sum computation.
+     */
+    public SharedData(ArrayList<Integer> array, int b) {
+        this.array = array;
+        this.b = b;
+    }
 
-	public int getB() 
-	{
-		return b;
-	}
+    /**
+     * Retrieves the winArray, which tracks elements contributing to the solution.
+     *
+     * @return A boolean array indicating the solution subset.
+     */
+    public boolean[] getWinArray() {
+        return winArray;
+    }
 
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    /**
+     * Updates the winArray to reflect the solution subset.
+     *
+     * @param winArray A boolean array indicating the solution subset.
+     */
+    public void setWinArray(boolean[] winArray) {
+        this.winArray = winArray;
+    }
 
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    /**
+     * Retrieves the array of integers.
+     *
+     * @return The array of integers being processed.
+     */
+    public ArrayList<Integer> getArray() {
+        return array;
+    }
 
+    /**
+     * Retrieves the target value for the subset sum computation.
+     *
+     * @return The target value.
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * Retrieves the flag indicating whether a solution has been found.
+     *
+     * @return True if a solution has been found, false otherwise.
+     */
+    public boolean getFlag() {
+        return flag;
+    }
+
+    /**
+     * Updates the flag to indicate whether a solution has been found.
+     *
+     * @param flag True if a solution is found, false otherwise.
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
